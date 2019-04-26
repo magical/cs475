7b28c392ec8332869f3de1a2f9eb12f2fcd96b67
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-04-25 18:02:11 -0700

    proj3: start stubbing out graindeer and graingrowth

--- proj3/grain.go
+++ proj3/grain.go
@@ -1,13 +1,18 @@
 package main
 
+import (
+	"math"
+	"math/rand"
+)
+
 var (
-	nowYear = 2019
-	nowMonh = 0
+	nowYear  = 2019
+	nowMonth = 0
 	// can change starting values
 	nowPrecip  float64
 	nowTemp    float64
-	nowHeight  float64
-	nowNumDeer int
+	nowHeight  float64 = 1
+	nowNumDeer int     = 1
 )
 
 // plus fourth something
@@ -57,4 +62,75 @@ func watchertemplate() {
 	// barrier
 	// print current state
 	// barrier
+}
+
+func watcher() {
+	for nowYear < 2025 {
+		// compute a temporary next-value for this quantity
+		// based on the current state of the simulation:
+		//. . .
+
+		// DoneComputing barrier:
+		//#pragma omp barrier
+		//. . .
+
+		// DoneAssigning barrier:
+		//#pragma omp barrier
+		//. . .
+
+		// DonePrinting barrier:
+		//#pragma omp barrier
+		//. . .
+	}
+
+}
+
+func TemperatureAndPrecipitation() {
+	ang := (30*nowMonth + 15) * (math.Pi / 180)
+
+	temp := avgTemp - ampTemp*math.Sin(ang)
+	rng := rand.New(rand.NewSource(0))
+
+	nowTemp = temp + ranf(rng, -randomTemp, randomTemp)
+
+	precip := avgPrecipPerMonth + ampPrecipPerMonth*math.Sin(ang)
+	nowPrecip = precip + ranf(rng, -randomPrecip, randomPrecip)
+	if nowPrecip < 0 {
+		nowPrecip = 0
+	}
+}
+
+func GrainDeer() {
+	//  The Carrying Capacity of the graindeer is the number of inches of height of the grain. If the number of graindeer exceeds this value at the end of a month, decrease the number of graindeer by one. If the number of graindeer is less than this value at the end of a month, increase the number of graindeer by one.
+	//
+	carryingCapacity := nowHeight
+	nextNumDeer = nowNumDeer
+
+	if float64(nowNumDeer) > carryingCapacity {
+		nextNumDeer += 1
+	} else {
+		nextNumDeer -= 1
+	}
+}
+
+func GrainGrowth() {
+	//  Each month you will need to figure out how much the grain grows. If conditions are good, it will grow by GRAIN_GROWS_PER_MONTH. If conditions are not good, it won't.
+	//
+	//  You know how good conditions are by seeing how close you are to an ideal temperature (°F) and precipitation (inches). Do this by computing a Temperature Factor and a Precipitation Factor like this:
+
+	tempFactor := math.Exp(-square((nowTemp - midTemp) / 10))
+	precipFactor := math.Exp(-square((nowPrecip - midPrecip) / 10))
+
+	nowHeight += tempFactor * precipFactor * grainGrowsPerMonth
+	nowHeight -= nowNumDeer * oneDeerEatsPerMonth
+	if nowHeight < 0 {
+		nowHeight = 0
+	}
+
+}
+
+func square(x float64) float64 { return x * x }
+
+func ranf(rng *rand.Rand, low, high float64) float64 {
+	return rng.Float64()*(high-low) - low
 }
