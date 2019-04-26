63a85da549f3c3fc8a1280a19f4c28a960bea219
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-04-25 21:34:07 -0700

    proj2: inline the beizer curve constants
    these are only used once, and the names don't add any clarity to the
    code.  no need to declare a page full of constants for them.
    
    performed by manually running the C preprocessor on a snippet of the
    code.

--- proj2/main.c
+++ proj2/main.c
@@ -117,57 +117,16 @@ Height( int iu, int iv, int numnodes )
 	float bv2 = 3. * v * v * (1.-v);
 	float bv3 = v * v * v;
 
-#define TOPZ00	0.
-#define TOPZ10	1.
-#define TOPZ20	0.
-#define TOPZ30	0.
-
-#define TOPZ01	 1.
-#define TOPZ11	12.
-#define TOPZ21	 1.
-#define TOPZ31	 0.
-
-#define TOPZ02	0.
-#define TOPZ12	1.
-#define TOPZ22	0.
-#define TOPZ32	4.
-
-#define TOPZ03	3.
-#define TOPZ13	2.
-#define TOPZ23	3.
-#define TOPZ33	3.
-
-#define BOTZ00	0.
-#define BOTZ10	-3.
-#define BOTZ20	0.
-#define BOTZ30	0.
-
-#define BOTZ01	-2.
-#define BOTZ11	10.
-#define BOTZ21	-2.
-#define BOTZ31	0.
-
-#define BOTZ02	0.
-#define BOTZ12	-5.
-#define BOTZ22	0.
-#define BOTZ32	-6.
-
-#define BOTZ03	-3.
-#define BOTZ13	 2.
-#define BOTZ23	-8.
-#define BOTZ33	-3.
-
-
 	// finally, we get to compute something:
-	float top =       bu0 * ( bv0*TOPZ00 + bv1*TOPZ01 + bv2*TOPZ02 + bv3*TOPZ03 )
-	                + bu1 * ( bv0*TOPZ10 + bv1*TOPZ11 + bv2*TOPZ12 + bv3*TOPZ13 )
-	                + bu2 * ( bv0*TOPZ20 + bv1*TOPZ21 + bv2*TOPZ22 + bv3*TOPZ23 )
-	                + bu3 * ( bv0*TOPZ30 + bv1*TOPZ31 + bv2*TOPZ32 + bv3*TOPZ33 );
+	float top = bu0 * ( bv0* 0. + bv1* 1. + bv2* 0. + bv3* 3. )
+	          + bu1 * ( bv0* 1. + bv1*12. + bv2* 1. + bv3* 2. )
+	          + bu2 * ( bv0* 0. + bv1* 1. + bv2* 0. + bv3* 3. )
+	          + bu3 * ( bv0* 0. + bv1* 0. + bv2* 4. + bv3* 3. );
 
-	float bot =       bu0 * ( bv0*BOTZ00 + bv1*BOTZ01 + bv2*BOTZ02 + bv3*BOTZ03 )
-	                + bu1 * ( bv0*BOTZ10 + bv1*BOTZ11 + bv2*BOTZ12 + bv3*BOTZ13 )
-	                + bu2 * ( bv0*BOTZ20 + bv1*BOTZ21 + bv2*BOTZ22 + bv3*BOTZ23 )
-	                + bu3 * ( bv0*BOTZ30 + bv1*BOTZ31 + bv2*BOTZ32 + bv3*BOTZ33 );
+	float bot = bu0 * ( bv0* 0. + bv1*-2. + bv2* 0. + bv3*-3. )
+	          + bu1 * ( bv0*-3. + bv1*10. + bv2*-5. + bv3* 2. )
+	          + bu2 * ( bv0* 0. + bv1*-2. + bv2* 0. + bv3*-8. )
+	          + bu3 * ( bv0* 0. + bv1* 0. + bv2*-6. + bv3*-3. );
 
 	// if the bottom surface sticks out above the top surface
 	// then that contribution to the overall volume is negative
