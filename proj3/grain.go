94e747b8524e90e34fd9e2cbca05e40e2986516f
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-04-29 18:38:24 -0700

    proj3: replace barrier with a channel-based approach in Go version

--- proj3/grain.go
+++ proj3/grain.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
-	"sync"
 )
 
-var (
-	nowYear  = 2019
-	nowMonth = 0
-	// can change starting values
-	nowPrecip  float64
-	nowTemp    float64
-	nowHeight  float64 = 1
-	nowNumDeer int     = 1
-)
+type state struct {
+	Year  int
+	Month int
+
+	Precip  float64
+	Temp    float64
+	Height  float64
+	NumDeer int
+}
+
+// can change starting values
+var now = &state{
+	Year:    2019,
+	Month:   0,
+	Height:  1,
+	NumDeer: 1,
+}
 
 // plus fourth something
 
@@ -37,130 +44,99 @@ const (
 )
 
 func main() {
-	updateTemperatureAndPrecipitation()
+	updateTemperatureAndPrecipitation(now)
 	//printResults()
-	go GrainDeer()
-	go GrainGrowth()
+	deerInC := make(chan *state, 1)
+	grainInC := make(chan *state, 1)
+
+	deerC := make(chan int, 1)
+	grainC := make(chan float64, 1)
+
+	go GrainDeer(deerInC, deerC)
+	go GrainGrowth(grainInC, grainC)
 	go MyAgent()
-	Watcher() // increments year and month
-}
 
-func thing() {
-	for {
-		// compute next state
-		//
-		// barrier
-		//
-		// copy next state to now state
-		//
-		// barrier
-		//
-		// ... let watcher thread run ...
-		//
-		// barrier
-	}
-}
+	for now.Year < 2025 {
+		deerInC <- now
+		grainInC <- now
 
-func watchertemplate() {
-	// barrier
-	// barrier
-	// print current state
-	// barrier
-}
+		nextHeight := <-grainC
+		nextDeer := <-deerC
 
-func Watcher() {
-	for nowYear < 2025 {
-		// compute a temporary next-value for this quantity
-		// based on the current state of the simulation:
-		//. . .
-
-		// DoneComputing barrier:
-		barrier()
-
-		// DoneAssigning barrier:
-		barrier()
+		now.Height = nextHeight
+		now.NumDeer = nextDeer
 
 		//print results and increment time
 		printResults()
-		updateYear()
-		updateTemperatureAndPrecipitation()
-
-		// DonePrinting barrier:
-		barrier()
+		updateYear(now)
+		updateTemperatureAndPrecipitation(now)
 	}
-
 }
 
 func printResults() {
-	fmt.Println(nowYear, nowMonth, nowTemp, nowPrecip, nowNumDeer, nowHeight)
+	fmt.Println(now.Year, now.Month, now.Temp, now.Precip, now.NumDeer, now.Height)
 }
 
-func updateYear() {
-	nowMonth++
-	if nowMonth == 12 {
-		nowYear++
-		nowMonth = 0
+func updateYear(now *state) {
+	now.Month++
+	if now.Month == 12 {
+		now.Year++
+		now.Month = 0
 	}
 }
 
 var rng = rand.New(rand.NewSource(0))
 
-func updateTemperatureAndPrecipitation() {
-	ang := float64(30*nowMonth+15) * (math.Pi / 180)
+func updateTemperatureAndPrecipitation(now *state) {
+	ang := float64(30*now.Month+15) * (math.Pi / 180)
 
 	temp := avgTemp - ampTemp*math.Cos(ang)
-	nowTemp = temp + ranf(rng, -randomTemp, randomTemp)
+	now.Temp = temp + ranf(rng, -randomTemp, randomTemp)
 
 	precip := avgPrecipPerMonth + ampPrecipPerMonth*math.Sin(ang)
-	nowPrecip = precip + ranf(rng, -randomPrecip, randomPrecip)
-	if nowPrecip < 0 {
-		nowPrecip = 0
+	now.Precip = precip + ranf(rng, -randomPrecip, randomPrecip)
+	if now.Precip < 0 {
+		now.Precip = 0
 	}
 }
 
-func GrainDeer() {
+func GrainDeer(in <-chan *state, out chan<- int) {
 	//  The Carrying Capacity of the graindeer is the number of inches of height of the grain. If the number of graindeer exceeds this value at the end of a month, decrease the number of graindeer by one. If the number of graindeer is less than this value at the end of a month, increase the number of graindeer by one.
 	//
 	for {
-		carryingCapacity := nowHeight
-		nextNumDeer := nowNumDeer
+		now := <-in
+		carryingCapacity := now.Height
+		nextNumDeer := now.NumDeer
 
-		if float64(nowNumDeer) > carryingCapacity {
+		if float64(now.NumDeer) > carryingCapacity {
 			nextNumDeer -= 1
-		} else if float64(nowNumDeer) < carryingCapacity {
+		} else if float64(now.NumDeer) < carryingCapacity {
 			nextNumDeer += 1
 		}
 
-		barrier()
-
-		nowNumDeer = nextNumDeer
-
-		barrier()
-		barrier()
+		out <- nextNumDeer
 	}
 }
 
-func GrainGrowth() {
+func GrainGrowth(in <-chan *state, out chan<- float64) {
 	//  Each month you will need to figure out how much the grain grows. If conditions are good, it will grow by GRAIN_GROWS_PER_MONTH. If conditions are not good, it won't.
 	//
 	//  You know how good conditions are by seeing how close you are to an ideal temperature (°F) and precipitation (inches). Do this by computing a Temperature Factor and a Precipitation Factor like this:
 
 	for {
+		now := <-in
 		// this function peaks at 1 around midTemp, and peters off to 0 at plus or minus 10
-		tempFactor := math.Exp(-square((nowTemp - midTemp) / 10))
-		precipFactor := math.Exp(-square((nowPrecip - midPrecip) / 10))
+		tempFactor := math.Exp(-square((now.Temp - midTemp) / 10))
+		precipFactor := math.Exp(-square((now.Precip - midPrecip) / 10))
 
-		nextHeight := nowHeight
+		nextHeight := now.Height
 		nextHeight += tempFactor * precipFactor * grainGrowsPerMonth
-		nextHeight -= float64(nowNumDeer) * oneDeerEatsPerMonth
+		nextHeight -= float64(now.NumDeer) * oneDeerEatsPerMonth
 		if nextHeight < 0 {
 			nextHeight = 0
 		}
 
-		barrier()
-		nowHeight = nextHeight
-		barrier()
-		barrier()
+		out <- nextHeight
 	}
 }
 
@@ -170,39 +146,4 @@ func square(x float64) float64 { return x * x }
 
 func ranf(rng *rand.Rand, low, high float64) float64 {
 	return rng.Float64()*(high-low) - low
-}
-
-var (
-	mu    sync.Mutex
-	wg1   sync.WaitGroup
-	wg2   sync.WaitGroup
-	nwait int
-)
-
-func init() {
-	wg1.Add(numthreads)
-}
-
-const numthreads = 3
-
-func barrier() {
-	// two-phase barrier
-
-	mu.Lock()
-	nwait++
-	if nwait == numthreads {
-		wg2.Add(numthreads)
-	}
-	mu.Unlock()
-	wg1.Done()
-	wg1.Wait()
-
-	mu.Lock()
-	nwait--
-	if nwait == 0 {
-		wg1.Add(numthreads)
-	}
-	mu.Unlock()
-	wg2.Done()
-	wg2.Wait()
 }
