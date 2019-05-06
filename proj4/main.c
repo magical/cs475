6bffaf329bbeab32426c595f5ad8cf123d513448
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-05-06 13:36:58 -0700

    proj4: calculate speedup for CPU vs SIMD

--- proj4/main.c
+++ proj4/main.c
@@ -11,12 +11,8 @@
 #endif
 
 // ranges for the random numbers:
-const float XCMIN =	-1.0;
-const float XCMAX =	+1.0;
-const float YCMIN =	 0.0;
-const float YCMAX =	 2.0;
-const float RMIN  =	 0.5;
-const float RMAX  =	 2.0;
+const float RANDMIN =	-1.0;
+const float RANDMAX =	+1.0;
 
 // function prototypes:
 float		Ranf( float, float );
@@ -44,50 +40,61 @@ int main( int argc, char *argv[ ] )
 		}
 	}
 
-	TimeOfDaySeed( );		// seed the random number generator
-
-	omp_set_num_threads( numthreads );	// set the number of threads to use in the for-loop:`
+	srand(0);
+	//TimeOfDaySeed( );		// seed the random number generator
 
 	// better to define these here so that the rand() calls don't get into the thread timing:
-	float *xcs = (float*)calloc(arraysize, sizeof xcs[0]);
-	float *ycs = (float*)calloc(arraysize, sizeof ycs[0]);
+	float *arr1 = (float*)calloc(arraysize, sizeof arr1[0]);
+	float *arr2 = (float*)calloc(arraysize, sizeof arr2[0]);
 	float * rs = (float*)calloc(arraysize, sizeof rs[0]);
 
 	// fill the random-value arrays:
 	for( int n = 0; n < arraysize; n++ )
 	{
-		xcs[n] = Ranf( XCMIN, XCMAX );
-		ycs[n] = Ranf( YCMIN, YCMAX );
-		rs[n] = Ranf(  RMIN,  RMAX );
+		arr1[n] = Ranf( RANDMIN, RANDMAX );
+		arr2[n] = Ranf( RANDMIN, RANDMAX );
+		rs[n] = 0;
 	}
 
 	// get ready to record the maximum performance and the probability:
-	float maxPerformance = 0.;
+	double minSimdTime = INFINITY;
+	double minCpuTime  = INFINITY;
 
-	float out_cpu = NonSimdMulSum(xcs, ycs, arraysize);
 	// looking for the maximum performance:
+
+	float got_cpu = 0.;
 	for( int t = 0; t < NUMTRIES; t++ )
 	{
 		double time0 = omp_get_wtime( );
-
-		// XXX do simd here
-
-		float out_simd = SimdMulSum(xcs, ycs, arraysize);
-		if (fabs(out_simd - out_cpu) > 0.0001) {
-			fprintf(stderr, "warning: simd %f != cpu %f\n", out_simd, out_cpu);
-		}
-
-		//SimdMul(xcs, ycs, rs, arraysize);
-
-
+		//got_cpu = NonSimdMulSum(arr1, arr2, arraysize);
+		NonSimdMul(arr1, arr2, rs, arraysize);
 		double time1 = omp_get_wtime( );
-		double megaMulsPerSecond = (double)arraysize / ( time1 - time0 ) / 1000000.;
-		if( megaMulsPerSecond > maxPerformance ) {
-			maxPerformance = megaMulsPerSecond;
+		double d = time1 - time0;
+		if (d < minCpuTime) {
+			minCpuTime = d;
 		}
 	}
 
-	printf("%d\t%ld\t0\t%f\n", numthreads, arraysize, maxPerformance);
+	float got_simd = 0.;
+	for( int t = 0; t < NUMTRIES; t++ )
+	{
+		double time0 = omp_get_wtime( );
+		//got_simd = SimdMulSum(arr1, arr2, arraysize);
+		SimdMul(arr1, arr2, rs, arraysize);
+		double time1 = omp_get_wtime( );
+		double d = time1 - time0;
+		if (d < minSimdTime) {
+			minSimdTime = d;
+		}
+	}
+
+	if (fabs(got_simd - got_cpu) > 0.00001) {
+		fprintf(stderr, "warning: simd %f != cpu %f\n", got_simd, got_cpu);
+	}
+
+	double mulSumSpeedup = minCpuTime / minSimdTime;
+
+	printf("%d\t%ld\t%f\t%f\t%g\t%g\n", numthreads, arraysize, got_simd, mulSumSpeedup, minSimdTime, minCpuTime);
 
 	return 0;
 }
@@ -95,7 +102,7 @@ int main( int argc, char *argv[ ] )
 float Ranf( float low, float high )
 {
 	float r = (float) rand();		// 0 - RAND_MAX
-	float t = r  /	(float) RAND_MAX;	// 0. - 1.
+	float t = r  /  (float) RAND_MAX;	// 0. - 1.
 
 	return	 low  +  t * ( high - low );
 }
@@ -103,7 +110,7 @@ float Ranf( float low, float high )
 
 void TimeOfDaySeed( )
 {
-	struct tm y2k = { 0 };
+	struct tm y2k = { };
 	y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
 	y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
 
