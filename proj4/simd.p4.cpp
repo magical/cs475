11b79bcb3b1c7dbcfca35c640b8e5866e062eafa
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-05-06 21:33:13 -0700

    proj4: convince gcc not to butcher SimdMulSum
    reintroduce the for loop in SimdMulSum. this time,
    introduce a local register variable to hold the intermediate sum.
    this seems to be enough to convince gcc not to spill everything to the
    stack on every loop iteration.
    
    https://gcc.gnu.org/onlinedocs/gcc/Local-Register-Variables.html#Local-Register-Variables

--- proj4/simd.p4.cpp
+++ proj4/simd.p4.cpp
@@ -44,27 +44,24 @@ SimdMulSum( float *a, float *b, int len )
 	float sum[4] = { 0., 0., 0., 0. };
 	int limit = ( len/SSE_WIDTH ) * SSE_WIDTH;
 
-	if (0 < limit) {
-		int count = limit;
+	{
 		float *atmp = a, *btmp = b;
-		__asm
-		(
-			".att_syntax\n\t"
-			"movups %2,%%xmm2\n\t"
-		".loop%=:\n\t"
-			"movups	(%0), %%xmm0\n\t"	// load the first sse register
-			"movups	(%1), %%xmm1\n\t"	// load the second sse register
-			"mulps	%%xmm1, %%xmm0\n\t"	// do the multiply
-			"addps	%%xmm0, %%xmm2\n\t"	// do the add
-			"addq $16, %0\n\t"
-			"addq $16, %1\n\t"
-			"subl %4,%3\n\t"
-			"jg .loop%=\n\t"
-			"movups %%xmm2,%2\n\t"
-			: /* outputs */ "+r" (atmp), "+r" (btmp), "+m" (sum), "+r" (count)
-			: /* inputs */ "ir" (SSE_WIDTH), "m" (*atmp), "m" (*btmp)
-			: /* clobbers */ "xmm0", "xmm1", "xmm2"
-		);
+		register __int128 tmp __asm ("xmm2") = 0;
+		for (int i = 0; i < limit; i += SSE_WIDTH) {
+			__asm (
+				".att_syntax\n\t"
+				"movups	(%0), %%xmm0\n\t"	// load the first sse register
+				"movups	(%1), %%xmm1\n\t"	// load the second sse register
+				"mulps	%%xmm1, %%xmm0\n\t"	// do the multiply
+				"addps	%%xmm0, %2\n\t"	// do the add
+				"addq $16, %0\n\t"
+				"addq $16, %1\n\t"
+				: /* outputs */ "+r" (atmp), "+r" (btmp), "+x" (tmp)
+				: /* inputs */ "m" (*atmp), "m" (*btmp)
+				: /* clobbers */ "xmm0", "xmm1"
+			);
+		}
+		__asm ("movups %1,%0" : "=m" (sum) : "x" (tmp) : /*no clobbers*/);
 	}
 
 	float extra = 0;
